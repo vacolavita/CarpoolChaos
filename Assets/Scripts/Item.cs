afdf59920cea99fc4c387505819e3923fc53a05f
dcalitri <dcalitri@student.fitchburgstate.edu>
2024-01-24 16:56:32 -0500

    More attempts to get items to not spawn in walls.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,29 +7,35 @@ public class Item : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("Car")) 
-        { 
+        if (other.gameObject.CompareTag("Car"))
+        {
             Destroy(gameObject);
         }
 
-    }
-    
-    private void OnCollisionStay(Collision other)
-    {
-        if (other.gameObject.CompareTag("Wall"))
+        else
         {
-            transform.position = new Vector3(Random.Range(-16.0f, 16.0f), 1, Random.Range(-16.0f, 16.0f));
+            if (other.gameObject.CompareTag("Wall"))
+            {
+                Debug.Log("Somebody PLEASE get this item out of the wall!!!");
+                transform.position = transform.position + new Vector3(Random.Range(-16.0f, 16.0f), 1, Random.Range(-16.0f, 16.0f));
+            }
         }
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+
     }
 }
