b024d144505ba08332019ed6efd1163a198dceda
dcalitri <dcalitri@student.fitchburgstate.edu>
2024-01-30 11:23:52 -0500

    Added a vehicle select menu

--- Assets/Scripts/LevelSelectButton.cs
+++ Assets/Scripts/LevelSelectButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StartButton : MonoBehaviour
+public class LevelSelectButton : MonoBehaviour
 {
     private Button button;
     // Start is called before the first frame update
@@ -16,11 +16,11 @@ public class StartButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void PlayGame()
     {
-        Loader.Load(Loader.Scene.VehicleSelect);
+        Loader.Load(Loader.Scene.LevelSelect);
     }
 }
