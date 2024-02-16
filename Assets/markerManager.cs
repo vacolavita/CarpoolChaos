c30070d535f88da157ce1a4b994e0a5e9e92be0b
vacolavita <vacolavita@gmail.com>
2024-02-16 12:01:41 -0500

    marker color change

--- Assets/markerManager.cs
+++ Assets/markerManager.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEditor.Progress;
 
 public class markerManager : MonoBehaviour
 {
@@ -13,14 +14,20 @@ public class markerManager : MonoBehaviour
     public NavMeshAgent agent;
     public Transform car;
     public Vector3 dest;
+    public Movement car1;
+    private GameObject pyramid;
     void Start()
     {
+        pyramid = GameObject.Find("pyramid");
         agent = GetComponent<NavMeshAgent>();
+        car1 = GameObject.Find("Car").GetComponent<Movement>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        UpdateColor();
         agent.enabled = true;
         NavMeshPath path = new NavMeshPath();
         transform.localPosition = new Vector3(0, 0, 0);
@@ -31,6 +38,24 @@ public class markerManager : MonoBehaviour
         {
             Vector3 nextPoint = new Vector3(path.corners.ElementAt(1).x, transform.position.y, path.corners.ElementAt(1).z);
             transform.LookAt(nextPoint);
+        }
+
+    }
+
+    void UpdateColor()
+    {
+        if (car1.select == 0)
+        {
+            pyramid.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+
+        if (car1.select == 1)
+        {
+            pyramid.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        if (car1.select == 2)
+        {
+            pyramid.GetComponent<MeshRenderer>().material.color = new Color(0.50f, 0.92f, 1.00f);
         }
     }
 }
