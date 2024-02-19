c8c9a0a4afe01750be177073bd7b78115569723d
vacolavita <vacolavita@gmail.com>
2024-02-19 15:20:29 -0500

    art stuff

--- Assets/Scripts/Items/Tent.cs
+++ Assets/Scripts/Items/Tent.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Tent : MonoBehaviour
 {
     private Destination dest;
-    public GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
         dest = GetComponent<Destination>();
         dest.destType = Random.Range(1, 4);
+        Material m = GetComponent<MeshRenderer>().material;
+        if (dest.destType == 1) {
+            m.color = new Color(0.3f, 1, 0.3f);
+        }
+        if (dest.destType == 2)
+        {
+            m.color = new Color(1, 0.3f, 0.3f);
+        }
+        if (dest.destType == 3)
+        {
+            m.color = new Color(0.3f,0.3f,1);
+        }
     }
 
     // Update is called once per frame
