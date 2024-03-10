14c12fe871a81312ab99f36863ef15fc094803f0
vacolavita <vacolavita@gmail.com>
2024-03-10 00:21:46 -0500

    More UI

--- Assets/PassengersUI.cs
+++ Assets/PassengersUI.cs
@@ -9,9 +9,15 @@ public class PassengersUI : MonoBehaviour
     public Sprite passenger;
     public Image[] images;
     public Image pas;
+    public Image[] buttons;
+    int[] colors;
     // Start is called before the first frame update
     void Start()
     {
+        colors = new int[3];
+        buttons = new Image[2];
+        buttons[0] = GetComponentsInChildren<Image>()[0];
+        buttons[1] = GetComponentsInChildren<Image>()[1];
         images = new Image[movement.carryingCapacity];
         for (int i = 0; i < movement.carryingCapacity; i++) {
             Image img = Instantiate(pas).GetComponent<Image>();
@@ -24,6 +30,9 @@ public class PassengersUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < colors.Length; i++) {
+            colors[i] = 0;
+        }
         for (int i = 0; i < movement.carryingCapacity; i++)
         {
             if (movement.passengers[i] != null)
@@ -32,28 +41,114 @@ public class PassengersUI : MonoBehaviour
                 images[i].sprite = passenger;
                 if (movement.passengers[i].GetComponent<Passenger>().passengerType == 1)
                 {
-                    images[i].color = new Color(0, 1, 0, 0.5f);
+                    images[i].color = new Color(0, 1, 0, 0.4f);
+                    colors[0]++;
                 }
                 if (movement.passengers[i].GetComponent<Passenger>().passengerType == 2)
                 {
-                    images[i].color = new Color(1, 0.2f, 0.2f, 0.5f);
+                    images[i].color = new Color(1, 0.2f, 0.2f, 0.4f);
+                    colors[1]++;
                 }
                 if (movement.passengers[i].GetComponent<Passenger>().passengerType == 3)
                 {
-                    images[i].color = new Color(0.3f, 0.4f, 1, 0.5f);
+                    images[i].color = new Color(0.3f, 0.4f, 1, 0.4f);
+                    colors[2]++;
                 }
                 if (movement.select == movement.passengers[i].GetComponent<Passenger>().passengerType - 1)
                 {
-                    images[i].color = new Color(images[i].color.r, images[i].color.g, images[i].color.b, 0.8f);
+                    images[i].color = new Color(images[i].color.r, images[i].color.g, images[i].color.b, 0.9f);
                     images[i].rectTransform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
                 }
                 else {
                     images[i].rectTransform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                 }
+
             }
             else {
                 images[i].enabled = false;
             }
+        }
+        int c = 0;
+        foreach (var item in colors)
+        {
+            if (item > 0)
+            {
+                c++;
+            }
+        }
+        if (c > 1)
+        {
+            buttons[0].enabled = true;
+            buttons[1].enabled = true;
+
+
+
+
+            if (movement.select == 0) {
+                if (colors[1] > 0)
+                {
+                    buttons[1].color = new Color(1, 0.2f, 0.2f);
+                }
+                else {
+                    buttons[1].color = new Color(0.3f, 0.4f, 1f);
+                }
+                if (colors[2] > 0)
+                {
+                    buttons[0].color = new Color(0.3f, 0.4f, 1f);
+                }
+                else
+                {
+                    buttons[0].color = new Color(1, 0.2f, 0.2f);
+                }
+            }
+
+
+            if (movement.select == 1)
+            {
+                if (colors[2] > 0)
+                {
+                    buttons[1].color = new Color(0.3f, 0.4f, 1f);
+                }
+                else
+                {
+                    buttons[1].color = new Color(0.0f, 1f, 0.0f);
+                }
+                if (colors[0] > 0)
+                {
+                    buttons[0].color = new Color(0.0f, 1f, 0.0f);
+                }
+                else
+                {
+                    buttons[0].color = new Color(0.3f, 0.4f, 1f);
+                }
+            }
+
+
+            if (movement.select == 2)
+            {
+                if (colors[0] > 0)
+                {
+                    buttons[1].color = new Color(0.0f, 1f, 0.0f);
+                }
+                else
+                {
+                    buttons[1].color = new Color(1, 0.2f, 0.2f);
+                }
+                if (colors[1] > 0)
+                {
+                    buttons[0].color = new Color(1, 0.2f, 0.2f);
+                }
+                else
+                {
+                    buttons[0].color = new Color(0.0f, 1f, 0.0f);
+                }
+            }
+
+        }
+        else
+        {
+            buttons[0].enabled = false;
+            buttons[1].enabled = false;
         }
     }
 }
