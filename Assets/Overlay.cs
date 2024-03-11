35f5fbfd4568a4eb39ddcd19e137d5acec98408f
vacolavita <vacolavita@gmail.com>
2024-03-11 19:36:03 -0400

    Overlays

---
+++ Assets/Overlay.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Overlay : MonoBehaviour
+{
+    Camera cam;
+    public RawImage image;
+    RenderTexture tex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.targetTexture = tex;
+        image.texture = tex;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(tex);
+        tex = new RenderTexture(Screen.width, Screen.height, 16);
+        tex.Create();
+        cam.targetTexture = tex;
+        image.texture = tex;
+        tex.Release();
+    }
+}
