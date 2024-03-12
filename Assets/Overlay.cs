4de905dffea89cf1885f0373458467a3d0f7b452
vacolavita <vacolavita@gmail.com>
2024-03-11 22:32:44 -0400

    stuff

--- Assets/Overlay.cs
+++ Assets/Overlay.cs
@@ -14,7 +14,7 @@ public class Overlay : MonoBehaviour
         cam = GetComponent<Camera>();
         cam.targetTexture = tex;
         image.texture = tex;
-        image.color = new Color(1, 1, 1, 0.1f);
+        image.color = new Color(0.9f, 0.9f, 0.9f, 0.1f);
         
     }
 
