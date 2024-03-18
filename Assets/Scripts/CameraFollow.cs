1fde0d2fd1297be3f4fd76bb9275b6911fe97b19
vacolavita <vacolavita@gmail.com>
2024-03-18 15:41:36 -0400

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -12,21 +12,26 @@ public class CameraFollow : MonoBehaviour
     public Vector2 yBorders;
     void Start()
     {
-        transform.SetPositionAndRotation(new Vector3(cameraFollow.position.x + cameraDistance.x, cameraFollow.position.y + cameraDistance.y, cameraFollow.position.z + cameraDistance.z), transform.rotation);
-        transform.LookAt(cameraFollow);
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (cameraFollow != null)
+        {
+            transform.SetPositionAndRotation(new Vector3(cameraFollow.position.x + cameraDistance.x, cameraFollow.position.y + cameraDistance.y, cameraFollow.position.z + cameraDistance.z), transform.rotation);
+            transform.LookAt(cameraFollow);
+        }
+
         Vector3 dir = (cameraDistance).normalized;
         float dist = Mathf.Sqrt(Mathf.Pow(cameraDistance.x, 2) + Mathf.Pow(cameraDistance.z, 2));
 
         transform.SetPositionAndRotation(new Vector3(
-Mathf.Clamp(cameraFollow.position.x + cameraDistance.x + cameraOffset.x,xBorders.x,xBorders.y),
+Mathf.Clamp(cameraFollow.position.x + cameraDistance.x + cameraOffset.x, xBorders.x, xBorders.y),
 cameraFollow.position.y + cameraDistance.y + cameraOffset.y,
-Mathf.Clamp(cameraFollow.position.z + cameraDistance.z + cameraOffset.z,yBorders.x,yBorders.y)), transform.rotation);
-        
+Mathf.Clamp(cameraFollow.position.z + cameraDistance.z + cameraOffset.z, yBorders.x, yBorders.y)), transform.rotation);
+
     }
 }
