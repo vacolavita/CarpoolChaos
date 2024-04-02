749e52d22c742a6218d8a7dfdf059d0dfc52bd12
vacolavita <vacolavita@gmail.com>
2024-04-02 09:19:47 -0400

    more

--- Assets/MenuInput.cs
+++ Assets/MenuInput.cs
@@ -16,7 +16,7 @@ public class MenuInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(nav);
+        //Debug.Log(nav);
     }
 
     public void OnNavigate(InputValue value)
