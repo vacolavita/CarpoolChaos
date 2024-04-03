8cc0460feeffaa6df6724f94d9661a0fae5ccd97
vacolavita <vacolavita@gmail.com>
2024-04-03 17:39:27 -0400

    particle changes + train randomization

--- Assets/Train.cs
+++ Assets/Train.cs
@@ -6,17 +6,45 @@ public class Train : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody body;
+    public GameObject[] cars;
+    public int[] indexes;
+
+    //258
     void Start()
     {
+
         body = GetComponent<Rigidbody>();
+        Shuffle();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        body.MovePosition(transform.position + new Vector3(0, 0, 0.3f));
+        body.MovePosition(transform.position + new Vector3(0, 0, 0.25f));
         if (transform.position.z > 350) {
             transform.position += new Vector3(0, 0, -500);
+            Shuffle();
+        }
+
+    }
+
+    void Shuffle() {
+        reshuffle(indexes);
+        int dist = 117;
+        foreach (var item in indexes) {
+            cars[item].transform.localPosition = new Vector3(dist, 0, 0);
+            dist += 259;
+        }
+    }
+
+    void reshuffle(int[] texts)
+    {
+        for (int t = 0; t < texts.Length; t++)
+        {
+            int tmp = texts[t];
+            int r = Random.Range(t, texts.Length);
+            texts[t] = texts[r];
+            texts[r] = tmp;
         }
     }
 }
