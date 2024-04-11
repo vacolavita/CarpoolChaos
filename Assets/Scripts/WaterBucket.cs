2d02cf94cec7e57ce679f14641bb9e26288f3aa6
dcalitri <dcalitri@student.fitchburgstate.edu>
2024-04-11 11:38:05 -0400

    Made the gas fire a stage event

--- Assets/Scripts/WaterBucket.cs
+++ Assets/Scripts/WaterBucket.cs
@@ -7,12 +7,18 @@ public class WaterBucket : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+
     }
 }
